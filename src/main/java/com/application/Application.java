ad41b3701230aaf8966cce8888a285e56a589f63
govindsingh <govindsingh@localhost>
2022-08-28 20:56:15 +0530

    removed logger

--- src/main/java/com/application/Application.java
+++ src/main/java/com/application/Application.java
@@ -1,15 +1,12 @@
 package com.application;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		Logger logger = LoggerFactory.getLogger(Application.class);
-		logger.info("Weather application is starting up");
 		SpringApplication.run(Application.class, args);
 		
 	}
